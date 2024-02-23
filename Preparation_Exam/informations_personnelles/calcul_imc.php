e35744f3aaa2ed6d163c819028551ce522a333c5
Vianpyro <10519369+Vianpyro@users.noreply.github.com>
2024-02-23 12:40:24 -0500

    Ajout de placeholders

--- Preparation_Exam/informations_personnelles/calcul_imc.php
+++ Preparation_Exam/informations_personnelles/calcul_imc.php
@@ -1,11 +1,12 @@
 <?php
-if ($_SERVER['REQUEST_METHOD'] == "GET" && isset($_GET['taille']) && isset($_GET['poids'])) {
-    $taille = $_GET['taille'];
-    $poids = $_GET['poids'];
+header("Content-Type: text/html");
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET' && isset($_GET['poids']) && isset($_GET['taille'])) {
+    $poids = $_GET["poids"];
+    $taille = $_GET["taille"];
 
     // Calcul de l'IMC
     $imc = $poids / (($taille / 100) * ($taille / 100));
 
-    // Affichage
-    echo "<p>Votre Indice de Masse Corporelle (IMC) est : $imc</p>";
+    echo "Votre Indice de Masse Corporelle (IMC) est : $imc";
 }
